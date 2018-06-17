76a1c98b7ecf67873363a9773f87ec0eff162202
Nagaraju <dnr5dnr@gmail.com>
2018-06-17 17:32:28 +0530

    Added new test case TC6 and updated TC5 testing some thing

--- src/test/java/scripts/TC6.java
+++ src/test/java/scripts/TC6.java
@@ -6,13 +6,13 @@ import config.StartBrowser;
 import reuse.Functions;
 import util.Xls_Reader;
 
-public class TC5 extends StartBrowser{
+public class TC6 extends StartBrowser{
   @Test
-  public void testNewToursLoginExcel() throws Exception {
+  public void testNewToursGithub() throws Exception {
 	  Xls_Reader reader = new Xls_Reader("TestData/data.xlsx");
 	  String uName=reader.getCellData("Sheet1", "UserName", 2);
 	  String password=reader.getCellData("Sheet1", "Password", 2);
-	  System.out.println("I added this");
+	  
 	  Functions f = new Functions();
 	  f.newLogin(uName, password);
 	  f.newFlights();
