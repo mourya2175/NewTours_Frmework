321143155c6e4ab141decaaeaf33a00f5d0d7657
Nagaraju <dnr5dnr@gmail.com>
2019-12-19 16:04:27 +0530

    Testing github

--- src/test/java/scripts/TC4.java
+++ src/test/java/scripts/TC4.java
@@ -8,11 +8,11 @@ import reuse.Functions;
 public class TC4 extends StartBrowser{
   @Test
   public void testNewToursLogin() throws Exception {
-	  System.out.println("This is for testing");
+
 	  Functions f = new Functions();
 	  f.newLogin();
 	  f.newFlights();
 	  f.newLogOff();
-	  System.out.println("This is for testing end");
+
   }
 }
