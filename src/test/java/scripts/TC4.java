3d3eb2da7ceb729fc7906ad21577581476397827
Nagaraju <dnr5dnr@gmail.com>
2019-12-19 07:08:13 +0530

    Sample change

--- src/test/java/scripts/TC4.java
+++ src/test/java/scripts/TC4.java
@@ -8,7 +8,7 @@ import reuse.Functions;
 public class TC4 extends StartBrowser{
   @Test
   public void testNewToursLogin() throws Exception {
-	  
+	  System.out.println("This is for testing");
 	  Functions f = new Functions();
 	  f.newLogin();
 	  f.newFlights();
